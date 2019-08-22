42faf3facdc122441fe25c7392bc99f8d39b790c
Emily Tucker <emilytucker4@yahoo.com>
2019-08-22 11:44:09 -0400

    added empty vars as an out line of the api

---
+++ javascript/api.js
@@ -0,0 +1,17 @@
+function displayGifs() {
+    var shows = [];
+    var gif = $(this).attr("data-name");
+    var queryURL = ;
+$.ajax({
+    url : queryURL,
+    method : "GET"
+}).this(function(responce){
+    var showDiv = ;
+    var rating = ;
+    ratingP= $("<p>").text();
+    showDiv.append(ratingP);
+    var gifURL = responce.;
+    var gif2 =;
+})
+    
+}
