11b55c83cac30ccbf7819ff6a614546fad79895a
Emily Tucker <emilytucker4@yahoo.com>
2019-08-24 23:26:42 -0400

    moved functions around and added to the hopefuly show the gifs

--- javascript/api.js
+++ javascript/api.js
@@ -1,65 +1,75 @@
 $(document).ready(function () {
-var shows = ["24", "The Big Bang Theory", "Diners, Drive-Ins, and Dives", "Bones", "NCIS"]
+    var shows = ["24", "The Big Bang Theory", "Diners, Drive-Ins, and Dives", "Bones", "NCIS"]
+
+    function renderButtons() {
+        $("#buttonPlacement").empty();
+        for (var i = 0; i < shows.length; i++) {
+
+            var b = $("<button>");
+            b.addClass("btn btn-success");
+            b.attr("data-name", shows[i]);
+            b.text(shows[i]);
+            $("#buttonPlacement").append(b);
+        }
+    };
+    function addbtn() {
+        $("#add-show").on("click", function (event) {
+            event.preventDefault();
+            var showGif = $("#show-input").val().trim();
+            if (showGif == "") {
+                return false;
+            }
+            shows.push(showGif);
+            renderButtons();
+
+        })
+    };
     function displayGifs() {
-        var gif = $(this).attr("data-name");
-        var queryURL = "https//:api.giphy.com/v1/gifs/search?q=" + gif + "api_key=C1CkQBwcSSghFUE65oOAqwjHvaZnFNwj&limit=15";
-       
+        var shows = $(this).attr("data-name");
+        var queryURL = "https//:api.giphy.com/v1/gifs/search?q="+shows+"api_key=C1CkQBwcSSghFUE65oOAqwjHvaZnFNwj&limit=15";
+        console.log(queryURL);
         $.ajax({
             url: queryURL,
             method: "GET"
-        }).this(function (responce) {
+        }).then(function (responce) {
+            console.log(responce);
+            $("#gifPlacement").empty();
             //div to hold the gifs
             var showDiv = $("<div class ='gif'>");
             // get the rating
-            var rating = responce.data.rating ;
+            var rating = responce.data.rating;
             // maknig a place to put the rating on screen
             var ratingP = $("<p>").text("Rating: " + rating);
             //appending the rating
             showDiv.append(ratingP);
             // getting the gif
             var gifURL = responce.data.images_origial_url;
-           // making a place to put the gif
+            // making a place to put the gif
             var gif2 = $("<img>").attr("src", gifURL);
             // appending the gif
             showDiv.append(gif2);
             // 
+            $("#gifPlacement").append(showDiv)
         });
+    }
 
-    }
-    function renderButtons() {
-        $("#buttonPlacement").empty();
-        for (var i = 0; i < shows.length; i++){
-            // making a button div for each entry
-            var b = $("<button>");
-            // adding the class
-            b.addClass("show-btn");
-            //giving an attribute
-            b.attr("data-name", shows[i]);
-            // adding the text
-            b.text(shows[i]);
-            //adding the new button
-            $("#buttonPlacement").append(b);
-        }
-    }
-    $(document).on("click")
-    $("add-show").on("click", function (event) {
-        event.preventDefault();
-        var showGif = $("#show-input").val().trim();
-        if (showGif==""){
-            return false;
-        }
-        shows.push(showGif);
-        renderButtons();
-        
-    });
-    $(document).on("click", ".show-btn", displayGifs);
+    // $(document).on("click")
+
+
+   
     renderButtons();
-    var state = $(this).attr("data-state");
-      if (state === "still") {
-        $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "animate");
-      } else {
-        $(this).attr("src", $(this).attr("data-still"));
-        $(this).attr("data-state", "still");
-      }
-});
+    addbtn();
+     $(document).on("click", ".show-btn", displayGifs);
+    //make this a function
+    $(document).on("click", "image", function () {
+        var state = $(this).attr("data-state");
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+    
+}) 
